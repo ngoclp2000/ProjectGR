1ca6879c62946f719d34d84e0195cf7bbfab295c
trinhngoc2709 <nindaphi@gmail.com>
2022-07-24 04:07:54 +0700

    Set up project

---
+++ BackEnd/app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const cookieParser = require("cookie-parser");
+const express = require('express')
+const morgan = require('morgan');
+const helmet = require('helmet');
+const fs = require('fs');
+const path = require('path');
+
+const accessLogStream = fs.readFileSync("./logs/access.log",{
+    interval: "id",
+    path: path.join(__dirname, 'log')
+});
+const isProduction = process.env.NODE_ENV === 'production';
+const port = process.env.LISTENING_PORT || 8888;
+
+const app = express();
+
+app.use(cookieParser());
+app.use(morgan(isProduction ? morgan("combined", {stream : accessLogStream}): morgan("dev") ) );
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+
+
+
+
+app.use((error, req, res, next) => {
+    res.status(error.errorCode);
+    res.json({ message: error.message });
+});
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+});
+const server = app.listen(port);
